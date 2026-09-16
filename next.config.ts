9b0e28b6cc6a964d14ec6b95bd46eeee0ab96949
Palitax <levinwerres@googlemail.com>
2026-09-16 20:16:20 +0200

    fix(vercel): include @img and sharp in outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/api/**/*": ["./public/**/*"],
+    "/api/**/*": [
+      "./node_modules/@img/**/*",
+      "./node_modules/sharp/**/*",
+      "./public/**/*",
+    ],
   },
-  serverExternalPackages: ["sharp"],
+  serverExternalPackages: [
+    "sharp",
+    "@img/sharp-linux-x64",
+    "@img/sharp-libvips-linux-x64",
+  ],
 };
 
 export default nextConfig;
+
